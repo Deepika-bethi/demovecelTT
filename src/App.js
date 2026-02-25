4ce0978de171de5c6ba5a7181cfef4bb57e0da31
Deepika-bethi <23eg104d16@anurag.edu.in>
2026-02-25 11:06:46 +0530

    Updated router and frontend only version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+// import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Register from "./Register";
+// import Home from "./Home";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <h1>Iam APP</h1>
+
+//       <Routes>
+//         <Route path="/" element={<Register />} />
+//         <Route path="/reg" element={<Register />} />
+//         <Route path="/h" element={<Home />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./Register";
 import Home from "./Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <h1>Iam APP</h1>
 
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Home />} />
         <Route path="/reg" element={<Register />} />
-        <Route path="/h" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
